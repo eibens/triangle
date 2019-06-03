6c59e7d25b8ccb557d796813a18c0e033c66df1f
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2019-06-04 01:49:40 +0200

    WebGL render loop

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log('Λ')
+const draw = (gl, now) => {
+  console.log(now)
+  gl.clearColor(...[233, 30, 99, 255].map(x => x / 255))
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+const resize = gl => {
+  const oldWidth = gl.canvas.width
+  const newWidth = gl.canvas.clientWidth
+  if (oldWidth !== newWidth) gl.canvas.width = newWidth
+
+  const oldHeight = gl.canvas.height
+  const newHeight = gl.canvas.clientHeight
+  if (oldHeight !== newHeight) gl.canvas.height = newHeight
+
+  gl.viewport(0, 0, newWidth, newHeight)
+}
+
+const main = () => {
+  const gl = document.getElementById('canvas').getContext('webgl')
+  if (!gl) {
+    console.log('no webgl :(')
+    return
+  }
+  const loop = now => {
+    now /= 1000
+    resize(gl)
+    draw(gl, now)
+    requestAnimationFrame(loop)
+  }
+  loop()
+}
+main()
